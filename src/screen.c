12a3234373d60c3f7276ae0dac7f82c3518cc5f4
Sameer78984 <129592813+Sameer78984@users.noreply.github.com>
2026-06-23 13:01:38 +0500

    feat: added append buffer

--- src/screen.c
+++ src/screen.c
@@ -1,9 +1,13 @@
 #include "editor.h"
 #include <unistd.h>
+#include <string.h>
 
 void editorDrawRows() {
   for (int y = 0; y < E.screenRows; y++) {
-    write(STDOUT_FILENO, "~\r\n", 3);
+    write(STDOUT_FILENO, "~", 1);
+    if(y < E.screenRows - 1){
+      write(STDOUT_FILENO, "\r\n", 2);
+    }
   }
 }
 
@@ -14,4 +18,24 @@ void editorRefreshScreen() {
   editorDrawRows();
 
   write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
+// append buffer
+struct abuf{
+  char *b;
+  int len;
+};
+
+#define ABUF_INIT {NULL, 0}
+
+void abAppend(struct abuf *ab, const char* s, int len){
+  char* new = realloc(ab->b, ab->len + len);
+  if(new == NULL) return;
+  memcpy(&new[ab->len], s, len);
+  ab->b = new;
+  ab-> len += len;
+}
+
+void abFree(struct abuf *ab){
+  free(ab->b);
 }
